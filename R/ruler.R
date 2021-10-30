2fb763e7ef3dcdc7730f79b19c99840161184a68
Hadley Wickham <h.wickham@gmail.com>
2021-10-30 10:27:42 -0500

    Add ruler() and re-document

---
+++ R/ruler.R
@@ -0,0 +1,16 @@
+#' Measure the width of the console
+#'
+#' @param width Width of rule
+#' @export
+#' @examples
+#' ruler()
+ruler <- function(width = getOption("width")) {
+  x <- seq_len(width)
+  y <- rep("-", length(x))
+
+  y[x %% 5 == 0] <- "+"
+  y[x %% 10 == 0] <- crayon::bold(as.character((x[x %% 10 == 0] %/% 10) %% 10))
+
+  cat(y, "\n", sep = "")
+  cat(x %% 10, "\n", sep = "")
+}
